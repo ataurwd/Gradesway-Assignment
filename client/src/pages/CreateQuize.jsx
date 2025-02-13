67198dff6ee542e0a87422a3c2f713a7affdb765
ataurwd <ataurrahman24707@gmail.com>
2025-02-13 22:43:10 +0600

    complete the use login logic

--- client/src/pages/CreateQuize.jsx
+++ client/src/pages/CreateQuize.jsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import React from "react";
 
 const CreateQuize = () => {
   const handelFormSubmit = (e) => {
@@ -38,7 +37,7 @@ const CreateQuize = () => {
             />
           </div>
           <Button type="submit" className="w-full">
-            Login
+            Create Quize
           </Button>
         </form>
       </div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,46 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Login = () => {
-  const handleSubmit = (e) => {};
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate()
+
+  // get user data from the database
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:5000/user");
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const username = form.username.value;
+    const password = form.password.value;
+    const foundUser = users.find(
+      (user) => user.user === username && user.password === password
+    );
+
+    if (foundUser) {
+      Swal.fire({
+        title: "Login successful!",
+        icon: "success"
+      });
+      navigate("/dashboard/all-quize");
+    } else {
+      Swal.fire({
+        title: "Invalid username or password!",
+        icon: "error",
+      })
+    }
+  };
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
@@ -14,17 +51,21 @@ const Login = () => {
             <label className="block text-sm font-medium">Username</label>
             <Input
               type="text"
+              name="username"
               placeholder="Enter username"
               className="mt-1"
               required
+              defaultValue="teacher"
             />
           </div>
           <div>
             <label className="block text-sm font-medium">Password</label>
             <Input
               type="password"
+              name="password"
               placeholder="Enter password"
               className="mt-1"
+              defaultValue="123456"
               required
             />
           </div>
